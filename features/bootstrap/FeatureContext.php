1bcbf4cbc458b2301c3cfa753a3e27eada9268ba
tuka217 <a.walasek@gazeta.pl>
2017-04-10 14:05:14 +0200

    Update License

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Twigleaf package.
+ *
+ * (c) Lakion
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Behat\Behat\Context\BehatContext;
 
 class FeatureContext extends BehatContext
